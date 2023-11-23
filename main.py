76b978e05b40edeae0f79da664f703c341267c9d
michaelmaxschaefer <michael.max.schaefer@gmail.com>
2023-11-23 22:53:33 +0100

    gitignore added for books folder + created main.py to read book and count words

---
+++ main.py
@@ -0,0 +1,21 @@
+def main():
+    path = "books/frankenstein.txt"
+    file_contents = get_text(path)
+    count = count_words(file_contents)
+    print (f"{count} words")
+
+def get_text(path):
+    with open(path) as f:
+        return f.read()
+
+def count_words(file_contents):
+    words = file_contents.split()
+    number_of_words = 0
+    for word in words:
+        number_of_words = number_of_words + 1
+    return number_of_words   
+
+#def count_letter(file_contents):
+
+
+main()
