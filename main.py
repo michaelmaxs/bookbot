f126f8b77cf70b0d87a1fec5a4cbfaaac40d2531
michaelmaxs <michael.max.schaefer@gmail.com>
2023-11-26 23:01:31 +0100

    add count_letters function

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ def main():
     file_contents = get_text(path)
     count = count_words(file_contents)
     print (f"{count} words in file")
+    letters = count_letters(file_contents)
+    print (f"{letters}")
 
 def get_text(path):
     with open(path) as f:
@@ -15,7 +17,19 @@ def count_words(file_contents):
         number_of_words = number_of_words + 1
     return number_of_words   
 
-#def count_letter(file_contents):
-#this is a test
+def count_letters(file_contents):
+    lettercount = {}
+    for char in file_contents:
+        char = char.lower()
+        if char.isalpha():
+            if char in lettercount:
+                lettercount[char] = lettercount[char] + 1
+            else:
+                lettercount[char] = 1
+    return lettercount
+
+#def report_printing(file_contents):
+#    report_body = []
+
 
 main()
